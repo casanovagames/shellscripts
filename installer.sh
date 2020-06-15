61e4944926a667a51fa9a528e3efee8b9e6b16d1
Gabriel Casanova <48224013+casanovagames@users.noreply.github.com>
2020-06-15 17:41:34 -0300

    Added new applications and changed a few things.

--- installer.sh
+++ installer.sh
@@ -1,18 +1,13 @@
 # This is a post setup script that install all the programs
 # that i like to use simply and fast.
 
-su
-
-echo "Add the non free repos:"
-echo "deb http://deb.debian.org/debian/ buster main contrib non-free"
-nano /etc/apt/sources.list
-
 # Update repos and packages
+dpkg --add-architecture i386
 apt update -y
 apt upgrade -y
 
 # Install from repos
-apt install tiled gimp krita audacity simplescreenrecorder lmms git snapd flatpak gnome-software dosbox quadrapassel gnome-boxes plank playonlinux flameshot apt-transport-https curl gnome-software-plugin-flatpak steam -y
+apt install tiled gimp krita audacity simplescreenrecorder lmms git snapd flatpak gnome-software dosbox quadrapassel gnome-boxes plank playonlinux flameshot apt-transport-https curl gnome-software-plugin-flatpak neofetch steam ttf-mscorefonts-installer gnome-characters gnome-software-plugin-snap firmware-linux firmware-linux-nonfree firmware-ralink wireless-tools -y
 
 # Install brave browser
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
@@ -36,21 +31,18 @@ wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Rele
 apt update -y
 apt install lutris -y
 
-# Install WPS office
-wget http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/8865/wps-office_11.1.0.8865_amd64.deb -O wps-office.deb
+# Download DEBs
+wget -c http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9522/wps-office_11.1.0.9522.XA_amd64.deb
 
-wget http://kdl.cc.ksosoft.com/wps-community/download/8372/wps-office_11.1.0.8372_amd64.deb -O web-office-fonts.deb
+wget -c https://az764295.vo.msecnd.net/stable/a5d1cc28bb5da32ec67e86cc50f84c67cc690321/code_1.46.0-1591780013_amd64.deb
 
-sudo dpkg -i wps-office*.deb -y
-sudo apt-get install -f -y
+wget -c https://dl.discordapp.net/apps/linux/0.0.10/discord-0.0.10.deb
 
 # Install flatpaks
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.github.libresprite.LibreSprite com.orama_interactive.Pixelorama org.ppsspp.PPSSPP -y
 
 # Install snaps
-snap install blender --channel=2.79/stable --classic -y
-snap install cool-retro-term retroarch kdenlive spotify -y
-snap install code --classic -y
-
-
+snap install blender --channel=2.79/stable --classic
+snap install cool-retro-term --classic
+snap install retroarch kdenlive spotify
